c3fbcb296f1b8e70e7bc5a38d516859092c03c02
AbiyM1 <it.abiy.menberu19@gmail.com>
2022-04-24 18:19:48 +0300

    Feat: first phase done

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,12 +6,12 @@ const Main = () => {
   return (
     <div className="main">
       <div className="top_div">
-        <p>Adventure.</p>
+        <p>Adventure</p>
       </div>
       <div className="center_div">
         <div className="the">
           <span className='left'>the</span>
-          <span className='right'>With John Anderson</span>
+          <span className='right'>With Abiy Menberu</span>
         </div>
         <div className="wildlife">
           <span className="left">wild</span>
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,67 @@
-import React from 'react'
-import './Sidebar.scss'
+import React from "react";
+import "./Sidebar.scss";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+// import DeleteIcon from '@material-ui/icons/Menu';
 
 const Sidebar = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
-    <div className='sidebar'>Sidebar</div>
-  )
-}
+    <div className="sidebar">
+      <Button
+        id="basic-button"
+        aria-controls={open ? "basic-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+      >
+        {/* <DeleteIcon/> */}
+        <i class="fa-solid fa-bars menu"></i>
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem onClick={handleClose}>Option one</MenuItem>
+        <MenuItem onClick={handleClose}>Option two</MenuItem>
+        <MenuItem onClick={handleClose}>Option three</MenuItem>
+      </Menu>
+      <div className="social_media">
+        <div className="facebook">
+          <i class="fa-brands fa-facebook-f"></i>
+        </div>
+        <div className="twitter">
+          <i class="fa-brands fa-twitter"></i>
+        </div>
+        <div className="linkedin">
+          <i class="fa-brands fa-linkedin-in"></i>
+        </div>
+      </div>
+      <div className="page_count">01 <p>/05</p></div>
 
-export default Sidebar
+      <div className="nav_buttons">
+        <div className="prev">
+          <i class="fa-solid fa-angle-left"></i>
+        </div>
+        <div className="next">
+          <i class="fa-solid fa-angle-right"></i>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
