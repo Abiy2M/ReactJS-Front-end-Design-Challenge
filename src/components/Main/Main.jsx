870996d2471bf6eb891d9ff9a6ea9121e9ca1eed
AbiyM1 <it.abiy.menberu19@gmail.com>
2022-04-24 14:03:15 +0300

    Feat: finished the landing page

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -8,8 +8,17 @@ const Main = () => {
       <div className="top_div">
         <p>Adventure.</p>
       </div>
-      <div className="center_div"></div>
-      <div className="bottom_div"></div>
+      <div className="center_div">
+        <div className="the">
+          <span className='left'>the</span>
+          <span className='right'>With John Anderson</span>
+        </div>
+        <div className="wildlife">
+          <span className="left">wild</span>
+          <span className="right">life</span>
+        </div>
+        <div className="adventure">adventure in the forest</div>
+      </div>
       <Footer />
     </div>
   );
